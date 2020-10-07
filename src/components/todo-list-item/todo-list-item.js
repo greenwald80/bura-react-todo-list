542f0b9388faa80e90d8fa3cfaf93345cd397333
Alexander <greenwald80@gmail.com>
2020-10-07 17:12:27 +0300

    function using

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "./todo-list-item.css";
 
-//const TodoListItem = (props) => {
-//return <span>{props.label}</span>;
-//};
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "steelblue" : "black",
-    fontWeight: important ? "bold" : "normal",
-  };
-  return (
-    <span className="todo-list-item">
-      <span style={style} className="todo-list-item-label">
-        {label}
+export default class TodoListItem extends Component {
+  onLabelClick() {
+    console.log(`Done: ${this.props.label}`);
+  }
+
+  render() {
+    const { label, important = false } = this.props;
+    const style = {
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal",
+    };
+    return (
+      <span className="todo-list-item">
+        <span
+          style={style}
+          className="todo-list-item-label"
+          onClick={this.onLabelClick.bind(this)}
+        >
+          {label}
+        </span>
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
