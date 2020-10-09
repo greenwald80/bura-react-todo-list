f8e04c4ece87a93b2c27af6c83f6b2a44a9e78c5
Alexander <greenwald80@gmail.com>
2020-10-09 23:33:32 +0300

    added function for new item

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,16 +2,32 @@ import React, { Component } from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: "",
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className="item-add-form">
-        <button
-          className="btn  btn-outline-secondary"
-          onClick={() => this.props.onItemAdded("TEST")}
-        >
-          Add Item
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+        />
+        <button className="btn  btn-outline-secondary">Add</button>
+      </form>
     );
   }
 }
