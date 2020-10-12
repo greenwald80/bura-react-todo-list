4021f74affe50a2bdaeca7f29059b5e564ee5ed2
Alexander <greenwald80@gmail.com>
2020-10-12 18:30:25 +0300

    added filter for all,active,done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -21,6 +21,7 @@ export default class App extends Component {
       this.createTodoItem("Item 3"),
     ],
     term: "",
+    filter: "all", //active,done,all
   };
 
   createTodoItem(label) {
@@ -129,9 +130,26 @@ export default class App extends Component {
     this.setState({ term });
   };
 
+  filter(items, filter) {
+    switch (filter) {
+      case "all":
+        return items;
+      case "active":
+        return items.filter((item) => !item.done);
+      case "done":
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
+  }
+
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
   render() {
-    const { todoData, term } = this.state;
-    const visibleItems = this.search(todoData, term);
+    const { todoData, term, filter } = this.state;
+    const visibleItems = this.filter(this.search(todoData, term), filter);
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
     return (
@@ -139,7 +157,10 @@ export default class App extends Component {
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel onSearchChange={this.onSearchChange} />
-          <ItemStatusFilter />
+          <ItemStatusFilter
+            filter={filter}
+            onFilterChange={this.onFilterChange}
+          />
         </div>
         <TodoList
           todos={visibleItems}
