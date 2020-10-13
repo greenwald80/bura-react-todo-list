f40a312ed157233cb1b3e1ee8ab369f21331022e
Alexander <greenwald80@gmail.com>
2020-10-13 14:04:38 +0300

    modify

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -9,13 +9,18 @@ export default class ItemStatusFilter extends Component {
   ];
 
   render() {
-    const { filter,onFilterChange } = this.props;
+    const { filter, onFilterChange } = this.props;
 
     const buttons = this.buttons.map(({ name, label }) => {
       const isActive = filter === name; //активная вкладка соответствует названию фильтра из name
       const clazz = isActive ? "btn-info" : "btn-outline-secondary";
       return (
-        <button type="button" className={`btn ${clazz}`} key={name} onClick={()=>onFilterChange(name)}>
+        <button
+          type="button"
+          className={`btn ${clazz}`}
+          key={name}
+          onClick={() => onFilterChange(name)}
+        >
           {label}
         </button>
       );
